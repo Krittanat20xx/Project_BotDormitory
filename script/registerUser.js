f78e689a69afa733158794878aa9985272c09666
Krittanat20xx <91689135+Krittanat20xx@users.noreply.github.com>
2022-09-02 18:16:58 +0700

    armupdate

---
+++ script/registerUser.js
@@ -0,0 +1,7 @@
+let apiurl = "http://localhost:3333";
+const email = document.getElementById("email_user")
+const password = document.getElementById("password_user")
+const name = document.getElementById("name_user")
+const lastname = document.getElementById("lastname_user")
+const phone = document.getElementById("tel_user")
+const roomnumber = document.getElementById("roomnumber_user")
